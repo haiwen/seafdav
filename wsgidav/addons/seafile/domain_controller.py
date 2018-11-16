28eb8276aa86f4b8f09c89f9d0d2ece34dc2ba31
“cuihaikuo” <haikuo93@126.com>
2018-11-16 15:40:06 +0800

    Support login with contact_email.

--- wsgidav/addons/seafile/domain_controller.py
+++ wsgidav/addons/seafile/domain_controller.py
@@ -4,6 +4,9 @@ from pysearpc import SearpcError
 from seaf_utils import CCNET_CONF_DIR, SEAFILE_CENTRAL_CONF_DIR, multi_tenancy_enabled
 import wsgidav.util as util
 
+import seahub_db
+from seahub_db import Base
+
 _logger = util.getModuleLogger(__name__)
 
 class SeafileDomainController(object):
@@ -11,6 +14,7 @@ class SeafileDomainController(object):
     def __init__(self):
         pool = ccnet.ClientPool(CCNET_CONF_DIR, central_config_dir=SEAFILE_CENTRAL_CONF_DIR)
         self.ccnet_threaded_rpc = ccnet.CcnetThreadedRpcClient(pool, req_pool=True)
+        self.session_cls = seahub_db.init_db_session_class()
 
     def __repr__(self):
         return self.__class__.__name__
@@ -36,8 +40,22 @@ class SeafileDomainController(object):
 
         try:
             if self.ccnet_threaded_rpc.validate_emailuser(username, password) != 0:
-                return False
-        except:
+                if not self.session_cls:
+                    return False
+                # Assume that @username is a contact_email, get real email from seahub_db
+                session = self.session_cls()
+                profile_profile = Base.classes.profile_profile
+                q = session.query(profile_profile.user).filter(profile_profile.contact_email==username)
+                email = q.first()[0]
+                session.close()
+                if not email:
+                    return False
+                if self.ccnet_threaded_rpc.validate_emailuser(email, password) != 0:
+                    return False
+
+                username = email
+        except Exception as e:
+            print e
             return False
 
         try:
