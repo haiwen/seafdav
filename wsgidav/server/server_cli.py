a209d1c31306e0046db5b2bd6779090608066445
Xiangyue Cai <caixiangyue007@gmail.com>
2020-11-13 18:18:42 +0800

    use thread as worker (#41)

--- wsgidav/server/server_cli.py
+++ wsgidav/server/server_cli.py
@@ -531,7 +531,7 @@ class GunicornApplication(gunicorn.app.base.BaseApplication):
 def _run_gunicorn(app, config, mode):
     options = {
         'bind': '%s:%s' % (config.get('host'), config.get('port')),
-        'workers': config.get('workers'),
+        'threads': config.get('workers'),
         "pidfile": config.get('pidfile'),
         "timeout": config.get('timeout')
     }
